71ff292f1c8f45723feac97248dcc6a4bc1f4101
Evan Carlin <evan@carlin.com>
2017-04-19 11:39:25 -0700

    Can read in file

--- deadlockPreliminary/deadlock.c
+++ deadlockPreliminary/deadlock.c
@@ -7,6 +7,9 @@
 
 int main(int argc, char** argv){
 
+    FILE *fp;
+    char fileBuffer[10];
+
     if(argc != 2){
         printf("Usage: ./deadlock </path/to/input/file>\n");
         exit(1);
@@ -15,7 +18,21 @@ int main(int argc, char** argv){
     //TODO: Remove, just for testing
     printf("Input file %s\n",argv[1]);
 
-    
+    //Open the file
+    fp = fopen(argv[1], "r");
+    if(fp){
+
+        while(fgets(fileBuffer, 10, fp) != NULL){
+
+        printf("Line: %s", fileBuffer);
+        }
+
+        fclose(fp);
+    }
+    else{
+        printf("Error problem opening file\n");
+        exit(1);
+    }
 
     return 0;
 }
